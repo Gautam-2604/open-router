830948051b3446940ee6e9b39168b14e87122f70
Gautam-2604 <gaurisariag@gmail.com>
2026-06-12 22:12:50 +0530

    auth route done

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,7 +1,8 @@
-import { Elysia } from "elysia";
+import { app } from "./app";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+})).listen(3000);
