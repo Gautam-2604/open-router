1cf6dcdb1693ec47932d77776cc7f9d558cc0250
Gautam-2604 <gaurisariag@gmail.com>
2026-06-13 11:36:33 +0530

    auth routes done correctly

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -2,7 +2,11 @@ import { app } from "./app";
 import { cors } from '@elysiajs/cors'
 
 
+
 app.use(cors({
+    
     origin: 'http://localhost:3001',
     credentials: true,
+    
 })).listen(3000);
+
